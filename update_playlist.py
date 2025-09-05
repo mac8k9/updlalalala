e3d5d15f6b4b771a11b9df4ba4cc0cfedfff3410
mac8k9 <maclandandy@gmail.com>
2025-09-05 14:55:13 -0500

    Add files via upload

---
+++ update_playlist.py
@@ -0,0 +1,42 @@
+import requests
+
+urls_php = [
+    "https://53ffe34dfb.stream.api.streamlatam.com/hls-url.php?token=O80SW4-TyU6VVJnMmkLSXQWeGkC",
+    "https://da5aef90cb.stream.api.streamlatam.com/hls-url.php?token=C5qhwt-Qyh7bnJrPygwaTQadSg5",
+    "https://f0a29b69f0.stream.api.streamlatam.com/hls-url.php?token=TCCmyj-VF5Ja3x8SRo1ZyoNAxo8",
+	"https://dca610047d.stream.api.streamlatam.com/hls-url.php?token=DDOqZu-RFlFd19sThYpRDUdBBYg",
+	"https://2bbf7695f7.stream.api.streamlatam.com/hls-url.php?token=j8AeGe-aiVLY0JCMhg9WSUzeBg0",
+	"https://90b449c9f8.stream.api.streamlatam.com/hls-url.php?token=MVKpo0-TUtBdmplXBIocXAUFhIh",
+	"https://fe64287420.stream.api.streamlatam.com/hls-url.php?token=UiNPYo-VXREVlx9YxcIRCsOKhcA",
+	"https://807a284ada.stream.api.streamlatam.com/hls-url.php?token=PbBx5N-UH9IfjB4aBsgKAoLIRso",
+    # agrega más URLs aquí
+]
+
+def obtener_url_m3u8(url_php):
+    try:
+        resp = requests.get(url_php)
+        resp.raise_for_status()
+        data = resp.json()
+        url = data.get("url")
+        if url:
+            return url.replace("legacy.m3u8", "0/chunklist.m3u8")
+        else:
+            print(f"No se encontró la URL en la respuesta de {url_php}")
+            return None
+    except Exception as e:
+        print(f"Error al obtener URL de {url_php}: {e}")
+        return None
+
+def actualizar_playlists_individuales(urls_php):
+    for i, url_php in enumerate(urls_php, start=1):
+        url_m3u8 = obtener_url_m3u8(url_php)
+        if url_m3u8:
+            nombre_canal = f"ECDF{i}"
+            contenido = f"#EXTM3U\n#EXTINF:-1,{nombre_canal}\n{url_m3u8}\n"
+            nombre_archivo = f"{nombre_canal}.m3u"
+            with open(nombre_archivo, "w") as f:
+                f.write(contenido)
+            print(f"Archivo {nombre_archivo} actualizado.")
+
+if __name__ == "__main__":
+    actualizar_playlists_individuales(urls_php)
